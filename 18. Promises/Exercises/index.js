a2e32792c6074e6d96816d48e48a3ded129318e7
TrixOMG <nikitin.pavel.163@gmail.com>
2024-01-28 22:24:34 +0300

    Promises done

--- 18. Promises/Exercises/index.js
+++ 18. Promises/Exercises/index.js
@@ -1,36 +1,66 @@
 const countriesAPI = "https://restcountries.com/v2/all";
 const catsAPI = "https://api.thecatapi.com/v1/breeds";
 
-// const fetchCountiesData = fetch(countriesAPI)
-//   .then((response) => response.json())
-//   .then((data) => data);
-
-// const countriesData = () => {
-//   fetchCountiesData.then((a) => a);
-// };
-
-// async function fetchCountiesData() {
-//   let response = await fetch(countriesAPI);
-//   let data = await response.json();
-//   data = JSON.stringify(data);
-//   data = JSON.parse(data);
-//   return data;
-// }
-
-// function main() {
-//   let countriesData = fetchCountiesData();
-//   return countriesData;
-// }
-
-// console.log(main());
-let countriesData;
+//countriesAPI using fetch and print the name of country, capital, population and area.
 
 fetch(countriesAPI)
   .then((response) => response.json())
   .then((data) => {
-    countriesData = data;
-  })
-  .then(console.log(countriesData))
-  .catch((err) => console.error(err));
+    data.forEach((country) => {
+      // console.log(
+      //   `Country name: ${country.name} Capital: ${country.capital} Population: ${country.population} Area: ${country.area}`
+      // );
+    });
+  });
 
-// console.log(countriesData);
+// Print out all the cat names in to catNames variable.
+let catsData = [];
+fetch(catsAPI)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      catsData.push(data[i].name);
+    }
+    // console.log(catsData);
+    // console.log(data);
+  });
+
+// Find the average weight of each cat in metric unit
+let catsWeight = [];
+fetch(catsAPI)
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      weightsArray = data[i].weight.metric.match(/[0-9]+/g);
+
+      averageWeight =
+        weightsArray.reduce(
+          (a, b) => parseInt(a) + parseInt(b)
+        ) / weightsArray.length;
+
+      // console.log(averageWeight);
+    }
+  });
+
+// Read the countries api and count total number of languages in the world used as officials.
+const fetchCountriesData = async () => {
+  try {
+    const response = await fetch(countriesAPI);
+    const countries = await response.json();
+
+    const setOfLanguages = new Set();
+
+    countries.forEach((country) => {
+      country.languages.forEach((language) => {
+        setOfLanguages.add(language.name);
+      });
+    });
+
+    document.getElementsByTagName("body")[0].innerHTML =
+      setOfLanguages.size;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+fetchCountriesData();
