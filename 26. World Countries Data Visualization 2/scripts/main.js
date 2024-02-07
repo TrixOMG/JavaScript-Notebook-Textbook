c690af5395a4b8d899ce82f6dd7190f16a1f6bc7
TrixOMG <nikitin.pavel.163@gmail.com>
2024-02-07 12:56:46 +0300

    Countries Data Viz 2 work in progress 2

--- 26. World Countries Data Visualization 2/scripts/main.js
+++ 26. World Countries Data Visualization 2/scripts/main.js
@@ -1,8 +1,23 @@
 const countriesAPI = "https://restcountries.com/v2/all";
 
-const countriesContainer = document.getElementById("countries-cards-container");
+const fetchedCountriesData = [];
 
-for(let i = 0; i < 50; i++){
+const countriesContainer = document.getElementById(
+  "countries-cards-container"
+);
+
+//Buttons
+const startingWordBtn = document.getElementById(
+  "starting-word-button"
+);
+const anyWordBtn = document.getElementById(
+  "any-word-button"
+);
+const changeOrderBtn = document.getElementById(
+  "change-order-button"
+);
+
+for (let i = 0; i < 50; i++) {
   const countryCard = document.createElement("div");
   countryCard.style.width = "5em";
   countryCard.style.height = "5em";
@@ -12,102 +27,83 @@ for(let i = 0; i < 50; i++){
 console.log(countriesContainer);
 
 function updateCaption(num) {
-  const numberParagraph = document.getElementById("total-num");
-  numberParagraph.innerText = "Total number of countries: " + num;
+  const numberParagraph =
+    document.getElementById("total-num");
+  numberParagraph.innerText =
+    "Total number of countries: " + num;
 }
 
-function visualizeStats(data) {
+function visualizeStats(type, search, order) {
+  if (search === "") {
+    // Показываем все карточки
+    // fetchedCountriesData
+    return;
+  }
   //Clean the container
-  statisticsContainer.innerHTML = "";
+  countriesContainer.innerHTML = "";
+}
 
-  const maxValue = data[0][1];
+function toggleMainButtonsColors(btn) {
+  const nonActiveColor = "#895be6";
+  const activeColor = "#6b43bb";
 
-  for (member of data) {
-    //Container for all data
-    const fullStatsContainer =
-      document.createElement("div");
-    fullStatsContainer.classList.add(
-      "full-stats-container"
-    );
-
-    const name = document.createElement("p");
-    name.innerText = member[0];
-    const count = document.createElement("p");
-    count.innerText = member[1];
-
-    // Container for "line"
-    const lineOfStatsContainer =
-      document.createElement("div");
-    lineOfStatsContainer.classList.add(
-      "line-of-stats-container"
-    );
-    lineOfStatsContainer.style.width = "100%";
-    lineOfStatsContainer.style.height = "2em";
-
-    // "Line"
-    const lineOfStats = document.createElement("div");
-    lineOfStats.classList.add("line-of-stats");
-    lineOfStats.style.height = "100%";
-    lineOfStats.style.width =
-      (member[1] / maxValue) * 100 + "%";
-
-    //Fitting "line" in container
-    lineOfStatsContainer.appendChild(lineOfStats);
-
-    fullStatsContainer.appendChild(name);
-    fullStatsContainer.appendChild(lineOfStatsContainer);
-    fullStatsContainer.appendChild(count);
-    statisticsContainer.appendChild(fullStatsContainer);
+  switch (btn) {
+    case "starting-word-button":
+      startingWordBtn.style.backgroundColor = activeColor;
+      anyWordBtn.style.backgroundColor = nonActiveColor;
+      break;
+    case "any-word-button":
+      startingWordBtn.style.backgroundColor =
+        nonActiveColor;
+      anyWordBtn.style.backgroundColor = activeColor;
+      break;
+    default:
+      console.error(
+        "Wrong button id passed to toggleMainButtonsColors"
+      );
+      break;
   }
 }
 
-populationButton.addEventListener("click", () => {
-  fetchCountriesData("population");
+startingWordBtn.addEventListener("click", () => {
+  // TODO
+  toggleMainButtonsColors("starting-word-button");
 });
 
-langButton.addEventListener("click", () => {
-  fetchCountriesData("languages");
+anyWordBtn.addEventListener("click", () => {
+  // TODO
+  toggleMainButtonsColors("any-word-button");
 });
 
-const fetchCountriesData = async (type) => {
+changeOrderBtn.addEventListener("click", () => {
+  // TODO
+  // visualizeStats(,);
+});
+
+const fetchCountriesData = async () => {
   try {
     const response = await fetch(countriesAPI);
     const countries = await response.json();
 
-    numOfCountriesSubtitle.innerText = `Currently we have ${countries.length} countries`;
+    countries.forEach((country) => {
+      fetchedCountriesData.push(country.name);
+    });
 
-    if (type === "languages") {
-      const countriesWithLangs =
-        getTenMostSpokenLanguages(countries);
-      visualizeStats(countriesWithLangs);
-      updateCaption(type);
-    } else if (type === "population") {
-      const countriesWithPopulation =
-        getTenMostPopulatedCountries(countries);
-      visualizeStats(countriesWithPopulation);
-      updateCaption(type);
-    } else {
-      console.error("Wrong type passed");
-      return;
-    }
+    updateCaption(countries.length);
   } catch (err) {
     console.error(err);
     // In case service is unavailable
     // (In learning purposes only)
     console.log("Getting Data stored locally");
-    if (type === "languages") {
-      const countriesWithLangs =
-        getTenMostSpokenLanguages(countries_data);
-      visualizeStats(countriesWithLangs);
-      updateCaption(type);
-    } else if (type === "population") {
-      const countriesWithPopulation =
-        getTenMostPopulatedCountries(countries_data);
-      visualizeStats(countriesWithPopulation);
-      updateCaption(type);
-    } else {
-      console.error("Wrong type passed");
-      return;
-    }
+
+    countries_data.forEach((country) => {
+      fetchedCountriesData.push(country.name);
+    });
+
+    updateCaption(countries.length);
   }
 };
+
+toggleMainButtonsColors("starting-word-button");
+visualizeStats("starting-word", "", "a-z");
+fetchCountriesData();
