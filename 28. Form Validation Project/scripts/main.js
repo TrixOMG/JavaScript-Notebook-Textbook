0b90d8ea6a4e3cf63d7307ad5de0f373e007b1e2
TrixOMG <nikitin.pavel.163@gmail.com>
2024-02-13 19:21:22 +0300

    Form validation work in progress

--- 28. Form Validation Project/scripts/main.js
+++ 28. Form Validation Project/scripts/main.js
@@ -1,5 +1,36 @@
 function validateForm(e) {
   e.preventDefault();
+
+  // Get inputs
+  const firstName = document.getElementById(
+    "first-name-input"
+  );
+  const lastName = document.getElementById(
+    "last-name-input"
+  );
+  const email = document.getElementById("email-input");
+  const password = document.getElementById(
+    "password-input"
+  );
+  const phone = document.getElementById("phone-input");
+  const bio = document.getElementById("bio-input");
+
+  // Get error paragraphs
+  const firstNameError = document.getElementById(
+    "first-name-error"
+  );
+  const lastNameError = document.getElementById(
+    "last-name-error"
+  );
+  const emailError = document.getElementById("email-error");
+  const passwordError = document.getElementById(
+    "password-error"
+  );
+  const phoneError = document.getElementById("phone-error");
+  const bioError = document.getElementById("bio-error");
+
+  // Validate inputs
+
   // TODO
-  console.log("submit");
+  // [ ] generate regex for each input
 }
