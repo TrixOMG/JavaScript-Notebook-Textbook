5e444fbd0e6a1259c2506c50623e17dc8d6c7ded
TrixOMG <nikitin.pavel.163@gmail.com>
2024-02-14 13:38:01 +0300

    Form Validation Project done

--- 28. Form Validation Project/scripts/main.js
+++ 28. Form Validation Project/scripts/main.js
@@ -1,36 +1,84 @@
+// Get inputs
+const firstName = document.getElementById(
+  "first-name-input"
+);
+const lastName = document.getElementById("last-name-input");
+const email = document.getElementById("email-input");
+const password = document.getElementById("password-input");
+const phone = document.getElementById("phone-input");
+const bio = document.getElementById("bio-input");
+
+// Get error paragraphs
+const firstNameError = document.getElementById(
+  "first-name-error"
+);
+const lastNameError = document.getElementById(
+  "last-name-error"
+);
+const emailError = document.getElementById("email-error");
+const passwordError = document.getElementById(
+  "password-error"
+);
+const phoneError = document.getElementById("phone-error");
+const bioError = document.getElementById("bio-error");
+
+//RegEx's
+const firstNameRegEx = /^[a-zA-Z]{3,16}$/;
+const lastNameRegEx = /^[a-zA-Z]{3,16}$/;
+const emailRegEx =
+  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+const passwordRegEx = /^[a-zA-Z0-9_\-\.\@]{6,20}$/;
+const phoneRegEx =
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+const bioRegEx = /^[a-z0-9_-]{8,50}$/;
+
 function validateForm(e) {
   e.preventDefault();
+  const validationArray = [];
 
-  // Get inputs
-  const firstName = document.getElementById(
-    "first-name-input"
-  );
-  const lastName = document.getElementById(
-    "last-name-input"
-  );
-  const email = document.getElementById("email-input");
-  const password = document.getElementById(
-    "password-input"
-  );
-  const phone = document.getElementById("phone-input");
-  const bio = document.getElementById("bio-input");
+  const inputsArray = [
+    firstName,
+    lastName,
+    email,
+    password,
+    phone,
+    bio,
+  ];
 
-  // Get error paragraphs
-  const firstNameError = document.getElementById(
-    "first-name-error"
-  );
-  const lastNameError = document.getElementById(
-    "last-name-error"
-  );
-  const emailError = document.getElementById("email-error");
-  const passwordError = document.getElementById(
-    "password-error"
-  );
-  const phoneError = document.getElementById("phone-error");
-  const bioError = document.getElementById("bio-error");
+  const errorMessagesArray = [
+    firstNameError,
+    lastNameError,
+    emailError,
+    passwordError,
+    phoneError,
+    bioError,
+  ];
 
-  // Validate inputs
+  const regexArray = [
+    firstNameRegEx,
+    lastNameRegEx,
+    emailRegEx,
+    passwordRegEx,
+    phoneRegEx,
+    bioRegEx,
+  ];
 
-  // TODO
-  // [ ] generate regex for each input
+  for (let i = 0; i < inputsArray.length; i++) {
+    if (!inputsArray[i].value.match(regexArray[i])) {
+      errorMessagesArray[i].style.display = "block";
+      inputsArray[i].style.border = "2px solid #fd4841";
+    } else {
+      errorMessagesArray[i].style.display = "none";
+      inputsArray[i].style.border = "2px solid gray";
+      validationArray.push(true);
+    }
+  }
+
+  if (!(validationArray.length === 6)) return;
+
+  const allInputs = document.querySelectorAll("input");
+
+  for (const input of allInputs) {
+    input.style.border = "2px solid #21bf72";
+  }
 }
