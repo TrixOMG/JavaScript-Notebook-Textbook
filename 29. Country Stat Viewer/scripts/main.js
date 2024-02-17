69bec08b0f9ebb83f495b08a02a301d8da9b726c
TrixOMG <nikitin.pavel.163@gmail.com>
2024-02-17 22:49:24 +0300

    Country Stat Viewer work in progress 3

--- 29. Country Stat Viewer/scripts/main.js
+++ 29. Country Stat Viewer/scripts/main.js
@@ -1,26 +1,66 @@
-//TODO:
-//HTML:
-//[X] add arrow icons to sort buttons
-//CSS:
-//
-//JS:
-//
-//
-//
-
 //const cCard = document.getElementById("c-card");
 //cCard.classList.add("country-card");
 //const flag = document.createElement("img");
 //flag.src = cCard.flag;
 //cCard.appendChild(flag);
 
+//TODO
+//[ ] сверстать карточку страны
+//[ ] отобразить страны при помощи карточек
+//  [X] найти как отображать сфетченную картинку
+//    countries.forEach((c) => {
+//      const countryFlag = document.createElement("img");
+//      countryFlag.src = c.flag;
+//      cardsContainer.appendChild(countryFlag);
+//    });
+//[ ] настроить поиск по всем полям "стран"
+
 const countriesAPI = "https://restcountries.com/v2/all";
+
+const cardsContainer = document.getElementById("cards-grid");
+
+const fetchedCountriesData = [];
+let copiedCountriesData = [];
+
+function filterCountriesBySearch(text) {
+  copiedCountriesData = fetchedCountriesData;
+
+  if (text === "") {
+    showSearchMessage(text);
+    drawCountriesCards(text);
+    visualizeStats(text);
+    return;
+  }
+
+  copiedCountriesData = copiedCountriesData.filter((c) => {
+    //TODO
+    c.toLowerCase().includes(text)
+  });
+
+  visualizeStats(text);
+}
+
+searchInput.addEventListener("input", () => {
+  let searchText = searchInput.value.toLowerCase();
+  filterCountriesBySearch(searchText);
+});
 
 const fetchCountriesData = async () => {
   try {
     const response = await fetch(countriesAPI);
     const countries = await response.json();
-    console.log(countries);
+
+    countries.forEach((c) => {
+      fetchedCountriesData.push({
+        flag: c.flag,
+        languages: c.languages,
+        capital: c.capital,
+        name: c.name,
+        populaiton: c.population,
+      });
+    });
+
+    filterCountriesBySearch();
   } catch (err) {
     console.error(err);
     // In case service is unavailable
@@ -33,5 +73,5 @@ const fetchCountriesData = async () => {
   }
 };
 
-//fetchCountriesData();
+fetchCountriesData();
 //
