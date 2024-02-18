83b398f06bdbf30d34c59764c7eec718b970d540
TrixOMG <nikitin.pavel.163@gmail.com>
2024-02-18 19:38:38 +0300

    Country Stat Viewe work in progress 4

--- 29. Country Stat Viewer/scripts/main.js
+++ 29. Country Stat Viewer/scripts/main.js
@@ -13,35 +13,84 @@
 //      countryFlag.src = c.flag;
 //      cardsContainer.appendChild(countryFlag);
 //    });
-//[ ] настроить поиск по всем полям "стран"
+//
+//[X] настроить поиск по всем полям "стран" (18.02)
+//[X] проверить работу поиска (18.02)
+//
 
 const countriesAPI = "https://restcountries.com/v2/all";
 
 const cardsContainer = document.getElementById("cards-grid");
 
+const input = document.getElementById("main-input");
+
+const searchMessage = document.getElementById("search-message");
+
 const fetchedCountriesData = [];
 let copiedCountriesData = [];
+
+function showSearchMessage(text) {
+  if (text === "") {
+    searchMessage.innerText = "";
+    searchMessage.style.display = "none";
+    return;
+  }
+
+  searchMessage.innerText = `${copiedCountriesData.length} countries satisfied the search criteria`;
+  searchMessage.style.display = "block";
+}
+
+function drawCountriesCards(text) {
+  cardsContainer.innerHTML = "";
+
+  copiedCountriesData.forEach((country) => {
+    const countryCard = document.createElement("div");
+    countryCard.classList.add("country-card");
+    countryCard.innerHTML = `<p>${country.name}</p>`;
+    cardsContainer.appendChild(countryCard);
+  });
+}
+
+function visualizeStats(text) {
+  //TODO
+}
+
+function renderAll(pText) {
+  showSearchMessage(pText);
+  drawCountriesCards(pText);
+  visualizeStats(pText);
+}
 
 function filterCountriesBySearch(text) {
   copiedCountriesData = fetchedCountriesData;
 
   if (text === "") {
-    showSearchMessage(text);
-    drawCountriesCards(text);
-    visualizeStats(text);
+    renderAll(text);
     return;
   }
 
+  //filter countries by Name, Capital or Languages
   copiedCountriesData = copiedCountriesData.filter((c) => {
-    //TODO
-    c.toLowerCase().includes(text)
+    //by name or capital
+    if (c.name.toLowerCase().includes(text)) return c;
+    //in case there is no capital (undefined)
+    if (c.capital) {
+      if (c.capital.toLowerCase().includes(text)) {
+        return c;
+      }
+    }
+    //by language
+    for (const lang of c.languages) {
+      if (lang.name.toLowerCase().includes(text)) return c;
+    }
   });
+  console.log(copiedCountriesData);
 
-  visualizeStats(text);
+  renderAll(text);
 }
 
-searchInput.addEventListener("input", () => {
-  let searchText = searchInput.value.toLowerCase();
+input.addEventListener("input", () => {
+  let searchText = input.value.toLowerCase();
   filterCountriesBySearch(searchText);
 });
 
@@ -60,7 +109,11 @@ const fetchCountriesData = async () => {
       });
     });
 
-    filterCountriesBySearch();
+    //update total number of countries message
+    const message = document.getElementById("number-of-countries-p");
+    message.innerText = message.innerText.replace("???", countries.length);
+
+    filterCountriesBySearch("");
   } catch (err) {
     console.error(err);
     // In case service is unavailable
